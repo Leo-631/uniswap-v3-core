525865448772c79fbfb6979050a00fe3b3a9f570
Leo.182340 <381156104@qq.com>
2023-02-21 16:12:46 +0800

    第一次提交

---
+++ contracts/UniswapV3PoolDeployer.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.7.6;
+
+import './interfaces/IUniswapV3PoolDeployer.sol';
+
+import './UniswapV3Pool.sol';
+
+contract UniswapV3PoolDeployer is IUniswapV3PoolDeployer {
+    struct Parameters {
+        address factory;
+        address token0;
+        address token1;
+        uint24 fee;
+        int24 tickSpacing;
+    }
+
+    /// @inheritdoc IUniswapV3PoolDeployer
+    Parameters public override parameters;
+
+    /// @dev Deploys a pool with the given parameters by transiently setting the parameters storage slot and then
+    /// clearing it after deploying the pool.
+    /// @param factory The contract address of the Uniswap V3 factory
+    /// @param token0 The first token of the pool by address sort order
+    /// @param token1 The second token of the pool by address sort order
+    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+    /// @param tickSpacing The spacing between usable ticks
+    function deploy(
+        address factory,
+        address token0,
+        address token1,
+        uint24 fee,
+        int24 tickSpacing
+    ) internal returns (address pool) {
+        parameters = Parameters({factory: factory, token0: token0, token1: token1, fee: fee, tickSpacing: tickSpacing});
+        //new UniswapV3Pool{}创建合约时加入了salt,
+        //将 token0, token1, fee 作为输入，得到一个哈希值，并将其作为 salt 来创建合约
+        //因为指定了 salt, solidity 会使用 EVM 的 CREATE2 指令来创建合约。
+        //使用 CREATE2 指令的好处是，只要合约的 bytecode 及 salt 不变，那么创建出来的地址也将不变
+        pool = address(new UniswapV3Pool{salt: keccak256(abi.encode(token0, token1, fee))}());
+        //调用完UniswapV3Pool.sol的构造方法再删除参数
+        delete parameters;
+    }
+}
